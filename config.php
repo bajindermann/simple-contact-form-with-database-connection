0ed3daade863b741172c7532111e5e7874fbb0f9
bajindermann <bajinder.mann@gmail.com>
2022-01-06 16:29:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD","");
-define("DB_NAME","contactdata");
+define("DB_HOST","172.30.150.167");
+define("DB_USER","pavdeep");
+define("DB_PASSWORD","pavdeep");
+define("DB_NAME","pavdeepdb");
 $mysqli = new mysqli(DB_HOST,DB_USER, DB_PASSWORD,DB_NAME);
 ?>
